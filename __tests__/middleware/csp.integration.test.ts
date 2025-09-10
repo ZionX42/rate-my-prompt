7ba5555cb5b0ebdc5acc50bd414b8f9049af60e8
Rick <you@users.noreply.github.com>
2025-09-10 11:38:08 +0100

    fix: update CSP rules to allow 'unsafe-inline' for style-src and style-src-attr

--- __tests__/middleware/csp.integration.test.ts
+++ __tests__/middleware/csp.integration.test.ts
@@ -67,9 +67,9 @@ describe('CSP Middleware Integration', () => {
       /script-src-elem 'self' 'nonce-[^']+' https:\/\/js\.sentry-cdn\.com https:\/\/cdn\.jsdelivr\.net https:\/\/unpkg\.com https:\/\/prompts3\.appwrite\.network/
     );
     expect(csp).toMatch(
-      /style-src 'self' 'nonce-[^']+' https:\/\/fonts\.googleapis\.com https:\/\/cdn\.jsdelivr\.net/
+      /style-src 'self' 'unsafe-inline' 'nonce-[^']+' https:\/\/fonts\.googleapis\.com https:\/\/cdn\.jsdelivr\.net/
     );
-    expect(csp).toMatch(/style-src-attr 'nonce-[^']+' 'unsafe-inline'/);
+    expect(csp).toMatch(/style-src-attr 'unsafe-inline'/);
     expect(csp).toMatch(
       /img-src 'self' data: https: blob: https:\/\/images\.unsplash\.com https:\/\/avatars\.githubusercontent\.com/
     );
